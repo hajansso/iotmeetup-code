05dee28a77d43f38c71689f0a08f9d613920bfdf
wakesurfer <wakesurfer@users.noreply.github.com>
2017-12-02 16:15:31 +0100

    New device

--- posix/bin/run.sh
+++ posix/bin/run.sh
@@ -4,4 +4,4 @@
 # p2 = Provisioning File Password
 # p3 = Start mode [test/prod]
 #/home/pi/iotcs/csl/posix/bin/iotclient.out AAAAAAQB7RGB-A2.conf Password1 $1
-iotclient.out AAAAAAQB7RGB-A2.conf Password1 test
+./iotclient.out ./AAAAAAQB7RGB-A3.conf Password1 test
