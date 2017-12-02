94ebe3117831a43815ff7a0f31caa507e296589e
wakesurfer <wakesurfer@users.noreply.github.com>
2017-12-02 16:07:21 +0100

    Update

--- posix/bin/run.sh
+++ posix/bin/run.sh
@@ -3,4 +3,5 @@
 # p1 = Provisioning File
 # p2 = Provisioning File Password
 # p3 = Start mode [test/prod]
-/home/pi/iotcs/csl/posix/bin/iotclient.out /home/pi/iotcs/csl/posix/bin/AAAAAAQB7RGB-A2.conf Password1 $1
+#/home/pi/iotcs/csl/posix/bin/iotclient.out AAAAAAQB7RGB-A2.conf Password1 $1
+iotclient.out AAAAAAQB7RGB-A2.conf Password1 test
