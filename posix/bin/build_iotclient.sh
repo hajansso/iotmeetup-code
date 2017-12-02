686c01ff8466397f75b9523e95ba31c5b9469ffc
wakesurfer <wakesurfer@users.noreply.github.com>
2017-12-02 16:34:48 +0100

    Updates

--- posix/bin/build_iotclient.sh
+++ posix/bin/build_iotclient.sh
@@ -1,4 +1,4 @@
-#Byggscript för att skicka temp o fuktvärden till IoTCS
+#Script to build the iotclient with Adafruit sensor
 export IOTCS_OS_NAME="Raspbian GNU/Linux"
 export IOTCS_OS_VERSION="8"
 gcc -g -I../include -I../lib/arm -I./dht ./dht/pi_2_dht_read.c ./dht/common_dht_read.c ./dht/pi_2_mmio.c iotclient.c -o iotclient.out -Wl,-Bstatic -L../lib/arm -ldeviceclient -Wl,-Bdynamic -lssl -lcrypto -lm -lrt -lpthread
--- posix/bin/run_iotclient.sh
+++ posix/bin/run_iotclient.sh
@@ -3,5 +3,4 @@
 # p1 = Provisioning File
 # p2 = Provisioning File Password
 # p3 = Start mode [test/prod]
-#/home/pi/iotcs/csl/posix/bin/iotclient.out AAAAAAQB7RGB-A2.conf Password1 $1
 ./iotclient.out ./AAAAAAQB7RGB-A3.conf Password1 test
