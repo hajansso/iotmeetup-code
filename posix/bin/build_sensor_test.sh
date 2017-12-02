a19d7a86de8554f266071b061b8ade4e439f4233
wakesurfer <wakesurfer@users.noreply.github.com>
2017-12-02 17:40:56 +0100

    Revert "Update script"
    This reverts commit ace5ea690670a8b5d3ff1e29bb3bd4e3c589c7ff.

--- posix/bin/build_sensor_test.sh
+++ posix/bin/build_sensor_test.sh
@@ -1,4 +1,4 @@
 #Byggscript för att skicka temp o fuktvärden till IoTCS
 export IOTCS_OS_NAME="Raspbian GNU/Linux"
 export IOTCS_OS_VERSION="8"
-gcc -g -I../include -I../lib/arm -I./dht ./dht/pi_2_dht_read.c ./dht/common_dht_read.c ./dht/pi_2_mmio.c sensor_test.c -o sensor_test.out -Wl,-Bstatic -L../lib/arm -ldeviceclient -Wl,-Bdynamic -lssl -lcrypto -lm -lrt -lpthread
+gcc -g -I../include -I../lib/arm -I./dht ./dht/pi_2_dht_read.c ./dht/common_dht_read.c ./dht/pi_2_mmio.c test_sensor.c -o test_sensor.out -Wl,-Bstatic -L../lib/arm -ldeviceclient -Wl,-Bdynamic -lssl -lcrypto -lm -lrt -lpthread
