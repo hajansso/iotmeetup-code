ef7d8f2253716bae82044f4e2ddaa6ddca43ac94
hajansso <hajansso@gmail.com>
2017-12-04 17:29:08 +0100

    kopierat fil
    copierat fil

--- posix/bin/run_iotclient.sh
+++ posix/bin/run_iotclient.sh
@@ -3,4 +3,4 @@
 # p1 = Provisioning File
 # p2 = Provisioning File Password
 # p3 = Start mode [test/prod]
-./iotclient.out ./AAAAAAQB7RGB-A4.conf Password1 test
+./iotclient.out ./HJ Test Pi Device.conf Password1 welcome
